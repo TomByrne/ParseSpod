79f49f9aa399c6fd1800d36092de8ae8a39b4ef7
Thomas Byrne <tom.byrne@imagination.com>
2018-01-24 12:46:21 +1100

    -

--- bat/Build.bat
+++ bat/Build.bat
@@ -3,8 +3,12 @@ cd ../
 del dist\\ParseSpod.zip /Q
 rmdir dist\\temp /S /Q
 timeout 1
+
 mkdir dist\\temp
-xcopy src dist\\temp /S /I
+xcopy src dist\\temp\\src /S /I
+copy haxelib.json dist\\temp
+copy run.n dist\\temp
+
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('dist\\temp', 'dist\\ParseSpod.zip'); }"
 haxelib submit dist\\ParseSpod.zip
 rmdir dist\\temp /S /Q
